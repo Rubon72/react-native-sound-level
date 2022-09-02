c2ba941541d0efcbe256cf9279f1a860d75bd1a2
Misha <Rubon72@gmail.com>
2022-09-02 14:42:26 +0300

    update

--- android/src/main/java/com/punarinta/RNSoundLevel/RNSoundLevel.java
+++ android/src/main/java/com/punarinta/RNSoundLevel/RNSoundLevel.java
@@ -24,6 +24,11 @@ public class RNSoundLevel implements ReactPackage {
 
         return modules;
     }
+    
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
     /**
      * @param reactContext
